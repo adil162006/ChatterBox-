83f6dfec1cb801c7477a49ccf8a401d5a1c9d64b
Mohd Adil <shaikhmdadil191@gmail.com>
2025-09-15 20:16:55 +0530

    ready for deployment

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,7 +4,7 @@ import express from 'express';
 const router=express.Router();
 
 
-router.get("/sighnup",(req,res)=>{
+router.get("/signup",(req,res)=>{
     res.send("Signup endpoint");
 });
 router.get("/login",(req,res)=>{
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path'
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -10,8 +11,22 @@ const port=process.env.PORT || 3000;
 
 
 const app = express();
+
+const __dirname = path.resolve()
+
+
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
+
+
+// make ready for deployment
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
