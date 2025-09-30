739ef4e5ca73928dd0000c0763a4361f06cac342
Mohd Adil <shaikhmdadil191@gmail.com>
2025-09-30 18:35:02 +0530

    login and logout setup completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ export const useAuthStore = create((set,get)=>({
 authUser:null,
 isCheckingAuth:true,
 isSigningUp:false,
-
+  isLoggingIn: false,
 
 checkAuth: async () => {
     try {
@@ -31,7 +31,30 @@ checkAuth: async () => {
     }finally{
       set({isSigningUp:false})
     }
-  }
+  },
+  login: async (data)=>{
+      set({ isLoggingIn: true });
+      try {
+         const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+
+      toast.success("Logged in successfully");
+      } catch (error) {
+         toast.error(error.response.data.message);
+      }finally{
+        set({ isLoggingIn: false });
+      }
+  },
+    logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
+    }
+  },
 
 
 
